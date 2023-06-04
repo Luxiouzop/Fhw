1cf1e0bd96509e085efca5baea07e6df31144f09
Luxiouzop <82629194+Luxiouzop@users.noreply.github.com>
2023-06-04 16:03:07 +0800

    游戏场景更新（箭矢、箭靶）

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -14,41 +14,177 @@ bool GameScene::init()
 {
 	auto visibleSize = Director::getInstance()->getVisibleSize();
 
+	// 弓
 	ArmatureDataManager::getInstance()->addArmatureFileInfo("Export/bow.ExportJson");
 	m_bow = Armature::create("bow");
-	if (m_bow == NULL)
-	{
-		CCLOG("bow load error!");
-		return false;
-	}
 	this->addChild(m_bow, 10);
 	m_bow->setScale(0.05);
-	m_bow->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+	m_bow->setPosition(Vec2(100, visibleSize.height / 2));
 	//m_bow->getAnimation()->play("ready");
 
-	//
+	// 箭矢
+	playerArrow.arrow = Sprite::create("jian.png");
+	playerArrow.arrow->setScale(0.1, 0.1);
+	playerArrow.islegal = true;
+	playerArrow.arrow->setName("arrow");
+	playerArrow.arrow->setPosition(m_bow->getPosition());
+	this->addChild(playerArrow.arrow, 9);
+	playerArrow.arrow->setVisible(false);
+
+	// 风速
+	wind = 0;
+
+	// 背景云
+	auto cloud1 = Sprite::create("cloud.png");
+	cloud1->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+	cloud1->setName("cloud1");
+	this->addChild(cloud1, 1);
+
+	// 得分
+	playerScore = 0;
+	auto scoreLabel = Label::createWithTTF(StringUtils::format("Score:%d", playerScore), "fonts/arial.ttf", 24);
+	scoreLabel->setName("scoreLabel");
+	scoreLabel->setPosition(Vec2(visibleSize.width / 2, visibleSize.height - 100));
+	scoreLabel->setTextColor(Color4B::YELLOW);
+	this->addChild(scoreLabel, 11);
+
+	// 箭靶
+	addTarget();
+	addTarget();
+	addTarget();
+	addTarget();
+
+	// 鼠标回调
 	auto listener = EventListenerTouchOneByOne::create();
 	listener->setSwallowTouches(true);
 	listener->onTouchEnded = CC_CALLBACK_2(GameScene::onTouchEnd, this);
 	listener->onTouchBegan = CC_CALLBACK_2(GameScene::onTouchBegin, this);
+	listener->onTouchMoved = CC_CALLBACK_2(GameScene::onTouchMove, this);
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
+
+	// update
+	this->scheduleUpdate();
 
 	return true;
 }
 
 void GameScene::update(float delta)
 {
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	if (playerArrow.islegal == false)
+	{
+		// 箭矢移动、旋转
+		playerArrow.arrow->setPosition(playerArrow.arrow->getPosition() + 0.1 * playerArrow.velocity);
+		playerArrow.arrow->setRotation(-CC_RADIANS_TO_DEGREES(playerArrow.velocity.getAngle()));
+		// 每次更新根据 风向、重力 修正速度
+		playerArrow.velocity.y -= 1;
+		if (playerArrow.velocity.x - 0.01-wind > 0) {
+			playerArrow.velocity.x -= 0.05 + wind;
+		}
 
+		// 检测箭矢与箭靶碰撞
+		for (auto target : targetVector)
+		{
+			if (playerArrow.arrow->getBoundingBox().intersectsRect(target->getBoundingBox()))
+			{
+				// 箭矢重置
+				playerArrow.arrow->setVisible(false);
+				playerArrow.arrow->setPosition(m_bow->getPosition());
+				playerArrow.islegal = true;
+				// 得分增加
+				auto scoreLabel = (Label*)this->getChildByName("scoreLabel");
+				playerScore += 1;
+				scoreLabel->setString(StringUtils::format("Score:%d", playerScore));
+				// 箭靶命中后消失
+				auto actionDown = CallFunc::create(CC_CALLBACK_0(GameScene::removeTarget, this, target));
+				target->runAction(actionDown);
+				// 箭靶增加
+				auto actionAdd = CallFunc::create(CC_CALLBACK_0(GameScene::addTarget, this));
+				auto sequence = Sequence::create(DelayTime::create(0.75), actionAdd, nullptr);
+				this->runAction(sequence);
+
+			}
+		}
+
+		// 检测箭矢越界
+		if (playerArrow.arrow->getPositionX() > visibleSize.width||playerArrow.arrow->getPositionX()<0
+			||playerArrow.arrow->getPositionY()<0||playerArrow.arrow->getPositionY()>1500)
+		{
+			playerArrow.arrow->setVisible(false);
+			playerArrow.arrow->setPosition(m_bow->getPosition());
+			playerArrow.islegal = true;
+		}
+	}
+		
 }
 
 void GameScene::onTouchEnd(Touch* touch, Event* unused_event)
 {
-	m_bow->getAnimation()->play("ready");
+	if (playerArrow.islegal == true)
+	{
+		playerArrow.point2 = touch->getLocation();
+		playerArrow.velocity = playerArrow.point1 - playerArrow.point2;
+		auto len = playerArrow.velocity.getLength() / 2;
+		if (len > playerArrow.maxV) {
+			len = playerArrow.maxV;
+		}
+		playerArrow.velocity = playerArrow.velocity.getNormalized() * len;
+		CCLOG("%f,%f", playerArrow.velocity.x, playerArrow.velocity.y);
+		playerArrow.islegal = false;
+		playerArrow.arrow->setVisible(true);
+	}
 }
 
 bool GameScene::onTouchBegin(Touch* touch, Event* unused_event)
 {
-	touch->getLocation();
-	return true;
+	if (playerArrow.islegal == true)
+	{
+		playerArrow.point1 = touch->getLocation();
+		m_bow->getAnimation()->play("ready");
+		return true;
+	}
+	else 
+	{
+		return false;
+	}
+	
+}
+
+void GameScene::onTouchMove(Touch* touch, Event* unused_event)
+{
+	if (playerArrow.islegal == true)
+	{
+		auto point = touch->getLocation();
+		m_bow->setRotation(-CC_RADIANS_TO_DEGREES((playerArrow.point1 - point).getAngle()));
+	}
+}
+
+void GameScene::addTarget()
+{
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	auto target = Sprite::create("target1.png");
+	target->setScale(0.1);
+	auto randx = random(1, 9);
+	auto xx = 0;
+	if (randx <= 4) {
+		xx = TargetField1;
+	}
+	else if (randx <= 7) {
+		xx = TargetField2;
+	}
+	else {
+		xx = TargetField3;
+	}
+	auto yy = random(100, 700);
+
+	target->setPosition(xx, yy);
+	this->addChild(target, 5);
+	this->targetVector.pushBack(target);
+}
+
+void GameScene::removeTarget(Sprite* target)
+{
+	this->removeChild(target);
+	targetVector.eraseObject(target);
 }
 
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -9,6 +9,22 @@ using namespace cocos2d;
 using namespace cocostudio;
 using namespace cocos2d::ui;
 
+
+struct ArrowState
+{
+	Sprite* arrow;  // 箭矢精灵
+	bool islegal;  // 箭矢是否可用，可用为true
+	Vec2 velocity; // 箭矢速度
+	Vec2 point1;   // 确定箭矢速度的起始点
+	Vec2 point2;   // 确定箭矢速度的终点
+	float maxV = 200;  //最大速度
+};
+
+#define TargetField1 750
+#define TargetField2 850
+#define TargetField3 950
+
+
 class GameScene :public cocos2d::Scene
 {
 public:
@@ -18,12 +34,16 @@ public:
 	void update(float delta);
 	void onTouchEnd(Touch* touch, Event* unused_event);
 	bool onTouchBegin(Touch* touch, Event* unused_event);
+	void onTouchMove(Touch* touch, Event* unused_event);
+	void addTarget();
+	void removeTarget(Sprite* target);
 
 private:
 	Armature* m_bow;
-
-
-
+	ArrowState playerArrow;
+	float wind;
+	Vector<Sprite*> targetVector;
+	int playerScore;
 };
 
 #endif // !_Game_Scene_H
