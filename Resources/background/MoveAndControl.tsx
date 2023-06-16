93c5f7f216e488f63a8f838c014d773315de7ace
DESKTOP-7703P42\PatrickWang <patrick_wzy@sina.com>
2023-06-16 10:31:22 +0800

    1

--- Resources/background/MoveAndControl.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="MoveAndControl" tilewidth="32" tileheight="32" tilecount="240" columns="20">
- <image source="地图素材/MoveAndControl.png" width="640" height="400"/>
-</tileset>
